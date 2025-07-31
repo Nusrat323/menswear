1877013bbf06d1a720ea925c9d8bec25773bfa21
Nusrat323 <samanusrat18@gmail.com>
2025-07-31 16:54:29 +0600

    add

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+{/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+})*/}
+
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  base: '/',
+  plugins: [react(), tailwindcss()]
 })
+
